b560c05a3f9bf5dff4fa1ed41afb28b853dbc971
Denlich <muchalovych@gmail.com>
2024-02-09 21:11:06 +0100

    add routing between auth pages

--- auction-web/src/components/templates/login-page/LoginPage.tsx
+++ auction-web/src/components/templates/login-page/LoginPage.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import LoginForm from "./login-form";
 import * as styles from "./LoginPage.styles";
+import Link from "next/link";
 
 const RegisterPage = () => {
   return (
     <Box sx={styles.container}>
       <LoginForm />
+      <Typography variant="body2">
+        Doesn&apost have account?{" "}
+        <Link href={"/register"}>
+          <Typography variant="body2" color="primary.main" component="span">
+            Register
+          </Typography>
+        </Link>
+      </Typography>
     </Box>
   );
 };
--- auction-web/src/components/templates/register-page/RegisterPage.tsx
+++ auction-web/src/components/templates/register-page/RegisterPage.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import RegisterForm from "./register-form";
 import * as styles from "./RegisterPage.styles";
+import Link from "next/link";
 
 const RegisterPage = () => {
   return (
     <Box sx={styles.container}>
       <RegisterForm />
+      <Typography variant="body2">
+        Already have account?{" "}
+        <Link href={"/login"}>
+          <Typography variant="body2" color="primary.main" component="span">
+            Login
+          </Typography>
+        </Link>
+      </Typography>
     </Box>
   );
 };
