6b7727cf60c170121872a15cb907a137593c65f9
Denlich <muchalovych@gmail.com>
2024-02-10 20:18:35 +0100

    fix: remove useAuth from auth-layout

--- auction-web/src/components/templates/profile-page/components/settings/Settings.tsx
+++ auction-web/src/components/templates/profile-page/components/settings/Settings.tsx
@@ -5,16 +5,19 @@ import AuthService from "@/lib/services/auth/AuthService";
 import { Box, Button, Typography } from "@mui/material";
 import * as styles from "./Settings.styles";
 import DeleteAccountButton from "./DeleteAccountButton";
+import { useRouter } from "next/navigation";
 
 const Settings = () => {
   const { update } = useAuth();
+  const router = useRouter();
 
   return (
     <Box sx={styles.container}>
       <Button
         onClick={async () => {
-          await AuthService.logout();
-          await update();
+          await AuthService.logout()
+            .then(update)
+            .then(() => router.push("/login"));
         }}
         sx={styles.logout}
       >
